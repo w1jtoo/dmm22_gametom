95c7e1dae28c275925712ca155d59c100a17c1cf
kharlov.v <kharlov.v@skbkontur.ru>
2022-05-01 02:10:34 +0500

    not a wip

--- front/Map.gd
+++ front/Map.gd
@@ -1,11 +1,42 @@
 extends YSort
+onready var Grass = preload("GrassBlock.tscn")
+onready var Road = preload("RoadBlock.tscn")
+onready var CrossRoad = preload("CrossRoadBlock.tscn")
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_block()
+
+
+var BLOCK_SIZE = 512 / 4
+var SIZE = 8
+
+func set_block():
+	var xPos = 64
+	var yPos = 64
+	var oldRect = null
+
+	for i in range(SIZE):
+		for j in range(SIZE):
+			var block = generate_insance(i, j)
+			
+			block.position.x = xPos
+			block.position.y = yPos
+
+			add_child(block)
+			xPos += BLOCK_SIZE
+		yPos += BLOCK_SIZE
+		xPos = 64
+
+
+func generate_insance(x: int, y: int) -> Node:
+	if (y == (SIZE / 2) and x == (SIZE - 1)/ 2):
+		return CrossRoad.instance()
+	if (x == ((SIZE - 1) / 2)):
+		var road = Road.instance()
+		road.rotate(PI / 2)
+		road.translate(Vector2.RIGHT * BLOCK_SIZE)
+		return road
+	if (y == (SIZE / 2)):
+		return Road.instance()
+	return Grass.instance()
