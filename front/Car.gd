d326c83c6bca808e41fc2d7942a6a95e5772050d
finkrer <finkrer@gmail.com>
2022-05-01 03:49:14 +0500

    Add cars

--- front/Car.gd
+++ front/Car.gd
@@ -10,15 +10,21 @@ onready var blue = $BlueCar
 onready var green = $GreenCar
 onready var red = $RedCar
 
+var rng = RandomNumberGenerator.new()
+var direction: Vector2
+
 func _ready():
-	rotate(Vector2.RIGHT.angle())
-	position.x = 0
-	position.y = 0
+	rng.randomize()
+	var random = rng.randf_range(0, 3)
+	match int(random) % 3:
+		0: blue.show()
+		1: green.show()
+		2: red.show()
 
 
 func _process(delta):
 	pass 
 	
 func _physics_process(delta):
-	position.x += 10
+	position += direction * 10
 
--- front/Sync.gd
+++ front/Sync.gd
@@ -4,22 +4,34 @@ const Car = preload("Car.tscn")
 # var a = 2
 # var b = "text"
 
+
 func _ready():
-	_http_request_completed()
-	#var http_request = HTTPRequest.new()
-	#add_child(http_request)
-	#http_request.get("request_completed", self, "_http_request_completed")
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	http_request.connect("request_completed", self, "_http_request_completed")
 
 	# Perform the HTTP request. The URL below returns a PNG image as of writing.
-	#var error = http_request.request("https://via.placeholder.com/512")
-	#if error != OK:
-	#	push_error("Что-то поломалось")
+	var error = http_request.request("http://37.79.216.231:8000/lol.json")
+	if error != OK:
+		push_error("Что-то поломалось")
 
 
 # Called when the HTTP request is completed.
 # func _http_request_completed(result, response_code, headers, body):
-func _http_request_completed():
-	var car = Car.instance()
-	car.position.x = 0
-	car.position.y = 0
-	add_child(car)
+func _http_request_completed(result, response_code, headers, body):
+	var response = parse_json(body.get_string_from_utf8())
+	var cars = response.cars
+	for carData in cars:
+		var car = Car.instance()
+		car.position.x = carData.position.x
+		car.position.y = carData.position.y
+		if carData.direction == 0:
+			car.direction = Vector2.UP
+		if carData.direction == 1:
+			car.direction = Vector2.RIGHT
+		if carData.direction == 2:
+			car.direction = Vector2.DOWN
+		if carData.direction == 3:
+			car.direction = Vector2.LEFT
+		
+		add_child(car)
